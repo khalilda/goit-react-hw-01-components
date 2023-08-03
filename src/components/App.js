51f165343c4f51cc924dabad8ef4a968e42f0044
khalilda <100872154+khalilda@users.noreply.github.com>
2023-08-03 15:32:25 +0300

    KD

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,17 @@
-import profileInfo from '../user.json'
-import { ProfileCard } from './ProfileCard';
-import { ProfileList } from './ProfileList';
-
-
+import { Profile } from './Profile';
+import user from '../user.json';
 
 
 export const App = () => {
   return (
     <>
-    <ProfileCard/>
-    <ProfileList infos={profileInfo}/>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </>
   );
 };
---
+++ src/components/Profile.js
@@ -0,0 +1,31 @@
+import ProfileModule from '../components/Profile.module.css';
+
+export const Profile = user => {
+  const { username, tag, location, avatar, stats } = user;
+
+  return (
+    <div className={ProfileModule.profile}>
+      <div className={ProfileModule.description}>
+        <img src={avatar} alt="User avatar" className={ProfileModule.avatar} />
+        <p className={ProfileModule.name}>{username}</p>
+        <p className={ProfileModule.string}>@{tag}</p>
+        <p className={ProfileModule.string}>{location}</p>
+      </div>
+
+      <ul className={ProfileModule.stats}>
+        <li className={ProfileModule.item}>
+          <span className={ProfileModule.label}>Followers</span>
+          <span className={ProfileModule.quantity}>{stats.followers}</span>
+        </li>
+        <li className={ProfileModule.item}>
+          <span className={ProfileModule.label}>Views</span>
+          <span className={ProfileModule.quantity}>{stats.views}</span>
+        </li>
+        <li className={ProfileModule.item}>
+          <span className={ProfileModule.label}>Likes</span>
+          <span className={ProfileModule.quantity}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
